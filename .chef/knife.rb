25a33c42826baa76f1167c6661193a4d9a726c59
Olaf Luetkehoelter <oluetkeh@gmail.com>
2014-01-07 09:48:55 -0500

    initial install

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'oluetkeh'
+client_key               '/home/oluetkeh/chef-repo/.chef/oluetkeh.pem'
+validation_client_name   'chef-validator'
+validation_key           '/home/oluetkeh/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://sinefo'
+syntax_check_cache_path  '/home/oluetkeh/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '/home/oluetkeh/chef-repo/cookbooks' ]
