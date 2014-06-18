a34763e1fcb70fb6d8655dcf36d417103542a09c
Olaf Luetkehoelter <oluetkeh@gmail.com>
2014-06-18 11:51:31 -0400

    .

--- cookbooks/kylab_demo/recipes/demo.rb
+++ cookbooks/kylab_demo/recipes/demo.rb
@@ -1,12 +1,22 @@
-apt_package "nginx" do
-  action :install
+case node[:platform]
+  when "fedora", "centos"
+    execute "install nginx repository" do
+      command "rpm -i http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm"
+      # not_if { ::File.exists?("/tmp/#{rpm}")}
+      ignore_failure true
+    end
+    yum_package "nginx"
+  when "ubuntu"
+    apt_package "nginx" do
+      action :install
+    end
 end
 
 service "nginx" do
   action [ :enable, :start ]
 end
 
-template "/usr/share/nginx/www/index.html" do
+template "/usr/share/nginx/html/index.html" do
   source "index.html.erb"
   action :create
   mode "664"
@@ -22,7 +32,7 @@ if node['hostname'] == "puppy"
     gid "puppy"
   end
   
-  cookbook_file "/usr/share/nginx/www/puppy.jpg" do
+  cookbook_file "/usr/share/nginx/html/puppy.jpg" do
     source "puppy.jpg"
     action :create
     owner "puppy"
@@ -41,7 +51,7 @@ else
     gid "kitty"
   end
   
-  cookbook_file "/usr/share/nginx/www/kitty.jpg" do
+  cookbook_file "/usr/share/nginx/html/kitty.jpg" do
     source "kitty.jpg"
     action :create
     owner "kitty"
